5883f4ed6bbfc3d038eaba88de7250976cd19e76
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2023-08-11 09:47:38 -0500

    Add support for accepting TS and ES6 sources for the rule function
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

--- deps.ts
+++ deps.ts
@@ -1,9 +1,15 @@
 export {
-  Interpreter,
-} from "https://raw.githubusercontent.com/yorinasub17/JS-Interpreter-deno/v0.0.1/mod.ts";
-export { Octokit } from "npm:@octokit/rest@^20.0.0";
-export {
   crypto,
   toHashString,
 } from "https://deno.land/std@0.197.0/crypto/mod.ts";
 export * as hex from "https://deno.land/std@0.197.0/encoding/hex.ts";
+
+export { Octokit } from "npm:@octokit/rest@^20.0.0";
+import babel from "npm:@babel/core@^7.22.10";
+import babelPresetEnv from "npm:@babel/preset-env@^7.22.10";
+import babelPresetTypescript from "npm:@babel/preset-typescript@^7.22.5";
+export { babel, babelPresetEnv, babelPresetTypescript };
+
+export {
+  Interpreter,
+} from "https://raw.githubusercontent.com/yorinasub17/JS-Interpreter-deno/v0.0.1/mod.ts";

8d9ca0447efebaa3f4b9a3b43a1336a6aa0d067d
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2023-09-25 13:47:41 -0500

    Introduce node-config for sane application configuration
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

--- deps.ts
+++ deps.ts
@@ -13,10 +13,11 @@ export {
 export type { Middleware, Next } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 export { Octokit } from "npm:@octokit/rest@^20.0.0";
 export { Webhooks as GitHubWebhooks } from "npm:@octokit/webhooks@^12.0.3";
+import config from "npm:config@^3.3.9";
 import babel from "npm:@babel/core@^7.22.10";
 import babelPresetEnv from "npm:@babel/preset-env@^7.22.10";
 import babelPresetTypescript from "npm:@babel/preset-typescript@^7.22.5";
-export { babel, babelPresetEnv, babelPresetTypescript };
+export { babel, babelPresetEnv, babelPresetTypescript, config };
 
 export {
   Interpreter,

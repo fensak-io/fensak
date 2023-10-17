b26db15dfcb5bc6f83bfa36ece2b9e646ce16695
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2023-10-17 13:02:44 -0500

    fix: Add copyright to loki.ts file
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

--- scripts/add_copyright.sh
+++ scripts/add_copyright.sh
@@ -10,7 +10,7 @@ function add_copyright {
   do
     if ! grep -q Copyright $i
     then
-      cat copyright.txt $i >$i.new && mv $i.new $i
+      cat ./scripts/copyright.txt $i >$i.new && mv $i.new $i
     fi
   done
 

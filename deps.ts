40a74cf11fb3ebab54b8f592c5e4fdf00b30f62a
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2023-10-18 09:03:03 -0500

    fix: Bump reng to the latest version so rules have access to source branch metadata
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

--- deps.ts
+++ deps.ts
@@ -68,4 +68,4 @@ import _Ajv from "npm:ajv@^8.12.0";
 const Ajv = _Ajv as unknown as typeof _Ajv.default;
 export { Ajv };
 
-export * as reng from "npm:@fensak-io/reng@1.0.7";
+export * as reng from "npm:@fensak-io/reng@1.1.2";
